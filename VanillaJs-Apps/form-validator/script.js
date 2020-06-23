ef23bf344f75652506a7f542bbab52e8e2fbfd3f
Filipe Ribeiro <filipepaiva240@gmail.com>
2020-06-22 23:46:17 -0300

    Implement checkRequiredFields function

---
+++ VanillaJs-Apps/form-validator/script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+
+function checkRequiredFields(requiredFields) {
+  requiredFields.forEach(field => {
+    if (field.value.trim() === '') {
+      showError(field, `${getFieldName(field)} is required!`);
+    } else {
+      showSuccess(field);
+    }
+  })
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  checkRequiredFields([username, email, password, confirmPassword]);
+});
